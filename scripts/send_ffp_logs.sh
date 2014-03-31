1462c2c7d7b39eecff82762949c7e8061cdfc4ea
c0ding <me@martinsimon.me>
2014-03-31 15:33:55 +0200

    added send_ffp_logs.sh

---
+++ scripts/send_ffp_logs.sh
@@ -0,0 +1,3 @@
+#!/ffp/bin/sh
+
+send_gen_mail -m "Dear Martin,\n\nyour NAS has rebooted successfully.\nPlease find more details in the attached fun_plug logs.\n\nSincerely,\nYour DNS-325" -s "DNS-325_E-Mail_Alert" -a /mnt/HD/HD_a2/ffp.log
